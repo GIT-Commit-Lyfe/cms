dd22f34faa889a8f2f0943b8c2d6bcf7f6764b9c
Dhoni <ramadhoni@keyreply.com>
2021-09-08 10:59:17 +0700

    updaet ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [
     {
-      name: 'NuxtAppName',
-      exec_mode: 'cluster',
+      name: 'tws-cms',
+      exec_mode: 'fork',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
